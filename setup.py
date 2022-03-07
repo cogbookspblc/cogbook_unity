05ccf0fe8ad8730ca528264246df848d9e088799
Chintan Joshi <chintanjoshi1995@gmail.com>
2022-03-07 16:12:14 +0300

    update: bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setuptools.setup(
     name="cogbook_unity",
-    version="0.0.1",
+    version="0.0.2",
     author="CogBooks",
     description="Create Components through API",
     packages=['cogbook_unity'],
     classifiers=[
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8.10",
         "Operating System :: OS Independent",
     ],
 )
